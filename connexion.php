56e1521f744d02bc8cc1ae06d70795026d79550a
prince thierry loubayi myssié <caviaros123@gmail.com>
2022-11-21 11:41:15 +0100

    New stable Air France

---
+++ connexion.php
@@ -0,0 +1,19 @@
+<?php
+require_once("vue/vue_connexion.php");
+if (isset($_POST['seConnecter'])) {
+
+    $email = $_POST['email'];
+    $mdp = $_POST['mdp'];
+
+    $unUser = $unControleur->verifConnexion($email, $mdp);
+
+    if ($unUser == null) {
+        echo "<br> Vérifiez vos identifiants";
+    } else {
+        $_SESSION['email'] = $unUser['email'];
+        $_SESSION['nom'] = $unUser['nom'];
+        $_SESSION['prenom'] = $unUser['prenom'];
+        $_SESSION['role'] = $unUser['role'];
+        header("Location: index.php?page=0");
+    }
+}
---
+++ vue/vue_connexion.php
@@ -0,0 +1,19 @@
+<section style="padding-left: 590px;padding-right: 590px; padding-top: 120px;">
+    <div class="card" style="margin: 28px;margin-right: 0;margin-left: 0;">
+        <div class="card-body" style="margin: 3px;">
+            <h4 class="text-center card-title" style="margin: 28px;font-weight: bold;">Connexion au site de la scolarité</h4>
+
+            <form method="post" style="margin-bottom: 30px;">
+                <div class="mb-3"><input class="form-control form-control-lg" type="email" name="email" required placeholder="Email"></div>
+                <div class="mb-3"><input class="form-control form-control-lg" type="password" name="mdp" required placeholder="Mot de passe"></div>
+
+                <div class="container">
+                    <div class="row">
+                        <div class="col-auto col-md-6 m-auto"><button class="btn btn-danger d-block w-100" type="submit" name="Annuler" style="height: 50px;">Annuler</button></div>
+                        <div class="col-md-6"><button class="btn btn-primary d-block w-100" type="submit" name="seConnecter" value="Connexion" style="height: 50px;">Valider</button></div>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+</section>
